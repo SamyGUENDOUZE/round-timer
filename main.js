9d34abdb19d393360d70255e179a671d92891b24
Lucas <lucas@colliders.com>
2022-05-15 02:33:02 +0200

    Timer fonctionnel avec mise a jours par rapport au round, pause fonctionne, reset aussi , break et start ne fonctionne pas encore

--- main.js
+++ main.js
@@ -1,158 +1,123 @@
-var start = document.getElementById('start');
-var stop = document.getElementById('stop');
-var reset = document.getElementById('reset');
+// VARIABLE
+let start = document.getElementById('start');
+let stop = document.getElementById('stop');
+let reset = document.getElementById('reset');
+let setting = document.getElementById('setting')
+let roundCounter = document.getElementById('counter');
 
-var wm = document.getElementById('w_minutes');
-var ws = document.getElementById('w_seconds');
+let minuteTimer = document.getElementById('w_minutes');
+let secondeTimer = document.getElementById('w_seconds');
 
-var bm = document.getElementById('b_minutes');
-var bs = document.getElementById('b_seconds');
+let breakSeconde = document.getElementById('b_seconds');
+let breakMinute = document.getElementById('b_minutes');
 
-var boxing_bell = new Audio("sound/boxing_bell.mp3");
-var whistle = new Audio("sound/whistle_referee.mp3");
+let container = document.getElementById('shadow-container')
+let closeIcon = document.getElementById('close-icon')
 
+let minuteInput = document.getElementById('timer-minute')
+let secondeInput = document.getElementById('timer-seconde')
+let roundInput = document.getElementById('round-input')
+let breakMinuteInput = document.getElementById('break-minute-input')
+let breakSecondeInput = document.getElementById('break-seconde-input')
+
+let timerFormButton = document.getElementById('form-setting-timer')
+
+let boxing_bell = new Audio("sound/boxing_bell.mp3");
+
+let whistle = new Audio("sound/whistle_referee.mp3");
 //store a reference to a timer variable
-var startTimer;
+let intervalChecker;
 
-start.addEventListener('click', function(){
-    if(startTimer === undefined){
-        startTimer = setInterval(timer, 1000)
-        boxing_bell.play();
+// FUNCTION
+
+
+function Timer() {
+    if (minuteTimer.innerText <= 0 && roundCounter.innerText <= 0 && (secondeTimer.innerText <= 0 || secondeTimer.innerText === "00")){
+        resetAll()
+        return
+    }
+    if ( (secondeTimer.innerText <= 0 || secondeTimer.innerText === "00") && minuteTimer.innerText <= 0 && roundCounter.innerText > 0 ){
+        roundCounter.innerText--
+        minuteTimer.innerText = minuteInput.value;
+        secondeTimer.innerText = secondeInput.value;
+    }
+    if(secondeTimer.innerText === "00" || secondeTimer.innerText === "0" && minuteTimer.innerText > 0){
+        minuteTimer.innerText--
+        secondeTimer.innerText = "59";
+    }else {
+        secondeTimer.innerText--
+    }
+}
+
+
+function playBell() {
+    boxing_bell.play().then(() => {
+      intervalChecker =  setInterval(Timer, 1000)
+    });
+}
+
+function displayForm() {
+    container.classList.remove('hidden')
+}
+
+function closeForm() {
+    container.classList.add('hidden')
+}
+
+function resetAll(){
+    [minuteTimer, breakMinute,roundCounter].forEach(i => {
+        i.innerText = '0'
+    });
+    [secondeTimer, breakSeconde].forEach(i => {
+        i.innerText = "00"
+    })
+    stopTimer()
+}
+
+
+
+function stopTimer() {
+    clearInterval(intervalChecker)
+    intervalChecker = undefined;
+}
+
+function startTime(){
+    Timer()
+}
+
+// à revoir
+function timerSetting() {
+    if (intervalChecker === undefined) {
+        minuteTimer.innerText = minuteInput.value;
+        secondeTimer.innerText = secondeInput.value;
+
+        roundCounter.innerText = roundInput.value;
+
+        breakMinute.innerText = breakMinuteInput.value;
+        breakSecondeInput.innerText = breakSeconde.value;
+
+        closeForm();
+        playBell();
+
     } else {
-        alert("Timer is already running");
-    }
-})
-
-reset.addEventListener('click', function(){
-    wm.innerText = 3;
-    ws.innerText = "00";
-
-    bm.innerText = 1;
-    bs.innerText = "00";
-
-    document.getElementById('counter').innerText = 0;
-    stopInterval()
-    startTimer = undefined;
-})
-
-stop.addEventListener('click', function(){
-    stopInterval()
-    startTimer = undefined;
-
-    // PLay the bell sound to tell the end of session
-    whistle.play();
-})
-
-
-//Start Timer Function
-function timer(){
-    
-    //Work Timer Countdown
-    if(ws.innerText !== 0){
-        ws.innerText--;
-    } else if(wm.innerText !== 0 && ws.innerText === 0){
-        ws.innerText = 59;
-        wm.innerText--;
-        // progressBar configuration
-        const timerElement = document.getElementsByClassName("timer");
-        const progressBar = document.getElementById("progressBar");
-
-        let timerCounter = progressBar.max;
-
-        const interval = setInterval(() => {
-        if (timerCounter <= 1) {
-            clearInterval(interval);
-        }
-
-        timerCounter = timerCounter - 1;
-        timerElement.innerText = timerCounter;
-        progressBar.value = timerCounter;
-}, 1000);
-    }
-
-    //Break Timer Countdown
-    if(wm.innerText === 0 && ws.innerText === 0){
-        if(bs.innerText !== 0){
-            bs.innerText--;
-        } else if(bm.innerText !== 0 && bs.innerText === 0){
-            bs.innerText = 59;
-            bm.innerText--;
-            // progressBar configuration
-            const timerElement = document.getElementsByClassName("timer");
-            const progressBar = document.getElementById("progressBar");
-
-            let timerCounter = progressBar.max;
-
-            const interval = setInterval(() => {
-            if (timerCounter <= 1) {
-                clearInterval(interval);
-        }
-
-            timerCounter = timerCounter - 1;
-            timerElement.innerText = timerCounter;
-            progressBar.value = timerCounter;
-}, 1000);
-        }
-    }
-
-    //Increment Counter by one if one full cycle is completed
-    if(wm.innerText === 0 && ws.innerText === 0 && bm.innerText === 0 && bs.innerText === 0){
-        wm.innerText = 3;
-        ws.innerText = "00";
-
-        bm.innerText = 1;
-        bs.innerText = "00";
-
-        document.getElementById('counter').innerText++;
+        intervalChecker = undefined
+        timerSetting()
     }
 }
 
-//Stop Timer Function
-function stopInterval(){
-    clearInterval(startTimer);
-}
+// EVENT LISTENER
+
+setting.addEventListener('click', displayForm)
+
+closeIcon.addEventListener('click', closeForm)
+
+timerFormButton.addEventListener('click', timerSetting)
+
+reset.addEventListener('click', resetAll)
+
+stop.addEventListener('click', stopTimer)
+
+start.addEventListener('click', startTime)
 
 
 
-//Partie du script qui gère le déplacement d'image
-
-
-//
-// function deplace () {
-//     for (var i = 0; i < nbFormules; i++) {
-//       if (formules[i]["right"] > largeurG) {
-//         formules[i]["right"] = 0;
-//         formules[i]["top"] = Math.floor(Math.random()*hauteurG);
-//       } else
-//         formules[i]["right"] += 1;
-//       document.getElementById('g'+i).style.top = formules[i]["top"]+'px';
-//       document.getElementById('g'+i).style.right = formules[i]["right"]+'px'; //c'est ce qui permet le sens de déplacement de l'objet
-//     }
-//   }
-//
-  // function deplaceSur (id) {
-  //   //spécifier le nombre d'objets ici
-  //   nbFormules = 4;
-  //   //la dimension de l'objet
-  //   objectWidth = 100;
-  //   objectHeight = 100;
-  //   //dimension de la zone
-  //   largeurG = parseInt(document.getElementById(id).style.width.substr(0, document.getElementById(id).style.width.length-2));
-  //   hauteurG = parseInt(document.getElementById(id).style.height.substr(0, document.getElementById(id).style.height.length-2));
-  //   largeurG -= objectWidth;
-  //   hauteurG -= objectHeight;
-  //   formules = [];
-  //   for (var i=0; i < nbFormules; i++) {
-  //     formules[i] = [];
-  //     formules[i]["right"] = Math.floor(Math.random() * largeurG);
-  //     formules[i]["top"] = Math.floor(Math.random()*hauteurG);
-  //
-  //     document.write ('<span id="g'+i+'" style="position: absolute; right: '+formules[i]["right"]+'px; top: '+formules[i]["top"]+'px">');
-  //     // placer ici l'objet qui défile !
-  //     document.write ("<img src='goku_cloud_little.png'>");
-  //     document.write ('</span>');
-  //   }
-  //
-  //   intervalG = window.setInterval ("deplace()", 30);
-  //   // là où il y a le chiffre 30, c'est ce qui permet de jouer sur le temps
-  // }
